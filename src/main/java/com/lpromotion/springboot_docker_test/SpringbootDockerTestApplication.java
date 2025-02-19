25bc0e8c7516c8c3412709ad118f5a97e7a1a000
lpromotion <lpromotion00@gmail.com>
2025-02-20 01:22:26 +0900

    Initial commit

---
+++ src/main/java/com/lpromotion/springboot_docker_test/SpringbootDockerTestApplication.java
@@ -0,0 +1,13 @@
+package com.lpromotion.springboot_docker_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lpromotion/springboot_docker_test/SpringbootDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpromotion.springboot_docker_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
